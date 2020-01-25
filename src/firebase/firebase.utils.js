0db257c191b5eccac164d038d8dcf60668d951fe
Ion <ionutzme3@gmail.com>
2020-01-25 13:38:00 +0200

    authentocated user in firestore with email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,7 +3,6 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-    
         apiKey: "AIzaSyAV-vY9rEUJ4kQ0OiiaD6KdefeltWfwVhM",
         authDomain: "crwn-db-cfae7.firebaseapp.com",
         databaseURL: "https://crwn-db-cfae7.firebaseio.com",
@@ -11,8 +10,32 @@ const config = {
         storageBucket: "crwn-db-cfae7.appspot.com",
         messagingSenderId: "149646345425",
         appId: "1:149646345425:web:085924425c69bb180e31f0",
-        measurementId: "G-NQ1WQH57Q2"
-      
+        measurementId: "G-NQ1WQH57Q2"   
+};
+
+export const createUserProfileDocument = async(userAuth, additionalData) =>{
+    if(!userAuth) return;
+    const userRef = firestore.doc(`user/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+           await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+           })
+        }
+        catch(error){
+          console.log("error creating user", error.message);
+        }
+    }
+    return userRef;
 };
 
 firebase.initializeApp(config);
