76c7f97b56479db27d76c431f314639baffb8662
Ion <ionutzme3@gmail.com>
2020-01-30 13:56:39 +0200

    adding checkout page and checkout item component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route, Redirect} from "react-router-dom";
 import {connect} from "react-redux";
 import ShopPage from "./pages/shop/shop.component";
 import HomePage from "./pages/homepage/homepage.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from "./components/header/header.component";
 import {setCurrentUser} from "./redux/user/user.actions";
 import {selectCurrentUser} from "./redux/user/user.selectors";
@@ -49,6 +50,7 @@ class App extends React.Component{
         <Switch>
           <Route exact path = "/" component={HomePage} />
           <Route path = "/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route 
           exact 
           path = "/signin"  
